bf95596feaf5e7aa102c8dce5f6375d4009c834b
abreuna <123099805+abreuna@users.noreply.github.com>
2023-01-24 09:48:42 -0600

    Create hello.kt

---
+++ week01/hello.kt
@@ -0,0 +1,10 @@
+/**
+ * Abreuna Selmon, this is my first Kotlin file
+ */
+
+
+fun main() {
+    println("Hello, world!!!")
+    println("I am learning Kotlin")
+    println("I do not like rainy days, that make me sad.")
+}
