0e9ec1dd6bc3993cc1b5e2f53ddac1b46b65c5c8
abreuna <123099805+abreuna@users.noreply.github.com>
2023-01-29 22:46:32 -0600

    Create pseudocode.kt

---
+++ week01/pseudocode.kt
@@ -0,0 +1,25 @@
+//Part 1
+display "Enter first number"
+display "Enter second number"
+multiply first number * second number
+display result
+
+//Part 2
+display "Enter number"
+display "number entered is not a 5 or a 6" if number is not 5 or 6
+
+//Part 3
+display "Enter a number"
+if number is between 0 and 10 display "blue"
+if number between 10 and 20 display "red"
+if number is between 20 and 30 display "green"
+if number is not between 0 and 30 display "incorrect color option"
+
+//Part 4
+x < 20 
+display x = x*5 
+
+//Part 5 
+x = 0
+x = x + 2
+disply x
