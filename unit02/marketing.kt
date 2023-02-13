b00caf59299fb97aaf6eca8d55142443811b8f9e
abreuna <123099805+abreuna@users.noreply.github.com>
2023-02-12 23:16:51 -0600

    Create marketing.kt

---
+++ unit02/marketing.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var friendsCount = 500
+    if(friendsCount > 500)
+      {
+          println("Adopt a dog today!")
+          
+      }
+    
+    else
+      {
+          println("Buy Cat Food at 20% Off")
+      }
+}
