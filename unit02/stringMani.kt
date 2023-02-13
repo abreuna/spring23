752ec20008e8ff08a700616820d1b3a93dd0afae
abreuna <123099805+abreuna@users.noreply.github.com>
2023-02-12 23:43:09 -0600

    Create stringMani.kt

---
+++ unit02/stringMani.kt
@@ -0,0 +1,8 @@
+fun main() 
+{
+   var city = "Dallas"
+    println("The length of the city name" + city.length)
+    println(city.toUpperCase())
+    println(city.toLowerCase())
+    println(city[0])
+}
